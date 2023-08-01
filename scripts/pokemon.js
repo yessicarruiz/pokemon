ab63b4cdb0ebba53feaf151c832d1ce401a3fda1
Erica1912 <eri.ruiz@hotmail.com.ar>
2023-08-01 17:43:36 -0500

    add styles and filter

--- scripts/pokemon.js
+++ scripts/pokemon.js
@@ -1,7 +1,48 @@
 const $containerCharacters = document.getElementById("container-characters");
 let url = "https://pokeapi.co/api/v2/pokemon";
+let typeUrl = "https://pokeapi.co/api/v2/type/"
+
 
 async function loadCharacters(url) {
+    try {
+        let res = await fetch(url);
+        let json = await res.json();
+        let $template = "";
+        //console.log(res);
+        //console.log(json);  
+
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        for (let i = 0; i < json.results.length; i++) {
+            url_name = (json.results[i].name);
+            let url_pokemon = (json.results[i].url);
+            let resPok = await fetch(url_pokemon);
+            let jsonPok = await resPok.json();
+            console.log(url_pokemon)
+            
+            let buyCards = document.createElement('div');
+            buyCards.className = 'buyCards';
+            $template += `
+                <div class="buyCards" >
+                    
+                    <div class="CardsDetails">
+                        <p>${json.results[i].name}</p>
+                        <i class="fa-sharp fa-regular fa-heart"></i>
+                    </div>
+                    <img class = "imgPokemon" src = "${jsonPok.sprites.other.home.front_default}">
+                    <div class="CardsDetails">
+                        <p><b>${jsonPok.base_experience} Exp</b></p>
+                        <button class="buybutton">Buy</button>
+                    </div>
+                    
+                </div>`;
+            $containerCharacters.innerHTML = $template;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+async function typesButton(type){
     try {
         let res = await fetch(url);
         let json = await res.json();
@@ -39,6 +80,6 @@ async function loadCharacters(url) {
     } catch (err) {
         console.log(err);
     }
-}
 
+}
 loadCharacters(url);
